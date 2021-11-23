65b95ff1a2ed64bcdca1a6b0eb643f19c260c3bd
niqkleedu <dominique.p.zachery@okstate.edu>
2021-11-22 20:12:52 -0600

    second commit

--- calcy.py
+++ calcy.py
@@ -3,14 +3,23 @@ import math
 import parser
 import tkinter.messagebox
 
-root = calcy()
+root = Tk()
 root.title("Scientific Calculator")
 root.configure(background = "white")
 root.resizable(width = False, height=False)
-root.geometry("500x560+0+0)
+root.geometry("500x560+0+0")
 
 calc = Frame(root)
 calc.grid()
 
 menubar = Menu(calc)
-filemenu = Menu
+filemenu = Menu(menubar, tearoff =0)
+menubar.add_cascade(label = "File", menu = filemenu)
+filemenu.add_command(label = "Standard")
+filemenu.add_command(label = "Scientific")
+filemenu.add_separator()
+filemenu.add_command(label = "Exit")
+
+root.configure(menu=menubar)
+root.mainloop()
+
